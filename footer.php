9b1c0a6b0888d4e9eae8881608ef3fe9b444a98f
Julian <julian1986.producciones@gmail.com>
2021-06-07 02:06:51 -0300

    Subida de portfolio

---
+++ footer.php
@@ -0,0 +1,24 @@
+ <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4 py-5">
+                    <p class="text-left"><a href="index.html"><strong><u> ©Todos los derechos reservados <br>
+                                    2020</u></strong></a></p>
+
+
+                </div>
+                <div class="col-12 col-md-4 py-5 redes">
+                    <a href=https://wa.link/pv46yv""> <i class="fab fa-whatsapp text-success"></i></a>
+                    <a href="https://www.linkedin.com/in/yamila-cappari-2906a156/"> <i
+                            class="fab fa-linkedin-in text-primary"></i></a>
+
+                    <a href="https://www.instagram.com/yc_webdesigner/"><i class="fab fa-instagram text-danger"></i></a>
+
+                </div>
+                <div class="col-12 col-md-4 py-5">
+                    <p class="text-right">Patrocinado por <strong><br><a
+                                href="https://depcsuite.com/?v=d72a48a8ebd2">DePcSuite</a></strong></p>
+                </div>
+            </div>
+        </div>
+    </footer>
